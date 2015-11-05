00ef4c8d83cf97365d556af847a9db0c1c11a9e4
Julien Couraud <couraud.julien@gmail.com>
2015-11-05 13:02:05 +0100

    ajout datas.pl

---
+++ datas.pl
@@ -0,0 +1,158 @@
+/* 
+	Products 
+product(id, name, suppliers [], quantity). 
+*/
+
+product(1, alternateurT1, [1,2,3,4], 100).
+product(2, alternateurT2, [1,2,3,4], 100).
+product(3, alternateurT3, [1,2,3,4], 100).
+product(4, alternateurT4, [1,2,3,4], 100).
+product(5, alternateurT5, [1,2,3,4], 100).
+
+product(6, demarreurT1, [1,2,3,4], 100).
+product(7, demarreurT2, [1,2,3,4], 100).
+product(8, demarreurT3, [1,2,3,4], 100).
+product(9, demarreurT4, [1,2,3,4], 100).
+product(10, demarreurT5, [1,2,3,4], 100).
+
+product(11, amortisseurT1, [1,2,3,4], 100).
+product(12, amortisseurT2, [1,2,3,4], 100).
+product(13, amortisseurT3, [1,2,3,4], 100).
+product(14, amortisseurT4, [1,2,3,4], 100).
+product(15, amortisseurT5, [1,2,3,4], 100).
+
+product(16, suspensionT1, [1,2,3,4], 100).
+product(17, suspensionT2, [1,2,3,4], 100).
+product(18, suspensionT3, [1,2,3,4], 100).
+product(19, suspensionT4, [1,2,3,4], 100).
+product(20, suspensionT5, [1,2,3,4], 100).
+
+product(21, batterieT1, [1,2,3,4], 100).
+product(22, batterieT2, [1,2,3,4], 100).
+product(23, batterieT3, [1,2,3,4], 100).
+product(24, batterieT4, [1,2,3,4], 100).
+product(25, batterieT5, [1,2,3,4], 100).
+
+product(26, bougieT1, [1,2,3,4], 100).
+product(27, bougieT2, [1,2,3,4], 100).
+product(28, bougieT3, [1,2,3,4], 100).
+product(29, bougieT4, [1,2,3,4], 100).
+product(30, bougieT5, [1,2,3,4], 100).
+
+product(31, pneuT1, [1,2,3,4], 100).
+product(32, pneuT2, [1,2,3,4], 100).
+product(33, pneuT3, [1,2,3,4], 100).
+product(34, pneuT4, [1,2,3,4], 100).
+product(35, pneuT5, [1,2,3,4], 100).
+
+product(36, retroviseurT1, [1,2,3,4], 100).
+product(37, retroviseurT2, [1,2,3,4], 100).
+product(38, retroviseurT3, [1,2,3,4], 100).
+product(39, retroviseurT4, [1,2,3,4], 100).
+product(40, retroviseurT5, [1,2,3,4], 100).
+
+product(41, parebriseT1, [1,2,3,4], 100).
+product(42, parebriseT2, [1,2,3,4], 100).
+product(43, parebriseT3, [1,2,3,4], 100).
+product(44, parebriseT4, [1,2,3,4], 100).
+product(45, parebriseT5, [1,2,3,4], 100).
+
+product(46, radiateurT1, [1,2,3,4], 100).
+product(47, radiateurT2, [1,2,3,4], 100).
+product(48, radiateurT3, [1,2,3,4], 100).
+product(49, radiateurT4, [1,2,3,4], 100).
+product(50, radiateurT5, [1,2,3,4], 100).
+
+product(51, duriteT1, [1,2,3,4], 100).
+product(52, duriteT2, [1,2,3,4], 100).
+product(53, duriteT3, [1,2,3,4], 100).
+product(54, duriteT4, [1,2,3,4], 100).
+product(55, duriteT5, [1,2,3,4], 100).
+
+product(56, moteurT1, [1,2,3,4], 100).
+product(57, moteurT2, [1,2,3,4], 100).
+product(58, moteurT3, [1,2,3,4], 100).
+product(59, moteurT4, [1,2,3,4], 100).
+product(60, moteurT5, [1,2,3,4], 100).
+
+/*
+	Suppliers
+supplier(id, name, location []).
+*/
+
+supplier(1, Abax, [0,0]).
+supplier(2, Aspock, [0,0]).
+supplier(3, Acimex, [0,0]).
+supplier(4, Bardhal, [0,0]).
+supplier(5, Bendix, [0,0]).
+supplier(6, Brink, [0,0]).
+supplier(7, Champion, [0,0]).
+supplier(8, Cipa, [0,0]).
+supplier(9, Colaert, [0,0]).
+supplier(10, Denso, [0,0]).
+supplier(11, Diframa, [0,0]).
+supplier(12, Doga, [0,0]).
+supplier(13, Electrifil, [0,0]).
+supplier(14, Eram, [0,0]).
+supplier(15, Faab, [0,0]).
+supplier(16, Ferodo, [0,0]).
+supplier(17, Fichet, [0,0]).
+supplier(18, Francefix, [0,0]).
+supplier(19, Gabriel, [0,0]).
+supplier(20, Gys, [0,0]).
+supplier(21, Haacon, [0,0]).
+supplier(22, Haldex, [0,0]).
+supplier(23, Hitachi, [0,0]).
+supplier(24, Iskra, [0,0]).
+supplier(25, Jonesco, [0,0]).
+supplier(26, Jost, [0,0]).
+supplier(27, Klaxcar, [0,0]).
+supplier(28, Knorr, [0,0]).
+supplier(29, Koni, [0,0]).
+supplier(30, Kumho, [0,0]).
+supplier(31, Lecoy, [0,0]).
+supplier(32, Lugdunum, [0,0]).
+supplier(33, Luk, [0,0]).
+supplier(34, Meiwa, [0,0]).
+supplier(35, Mekra, [0,0]).
+supplier(36, MGA, [0,0]).
+supplier(37, Muller, [0,0]).
+supplier(38, Netman, [0,0]).
+supplier(39, Ngk, [0,0]).
+supplier(40, Norton, [0,0]).
+supplier(41, Osram, [0,0]).
+supplier(42, Promauto, [0,0]).
+supplier(43, Raufoss, [0,0]).
+supplier(44, Ringfeder, [0,0]).
+supplier(45, Rockinger, [0,0]).
+supplier(46, Rubbolite, [0,0]).
+supplier(47, Sachs, [0,0]).
+supplier(48, Sicli, [0,0]).
+supplier(49, Siepa, [0,0]).
+supplier(50, Sifam, [0,0]).
+
+/*
+	Events
+event(id, name, impact, radius).
+*/
+
+event(1, tornade, 0, 0).
+event(2, tsunami, 0, 0).
+event(3, innondations, 0, 0).
+event(4, eruption_volcanique, 0, 0).
+event(5, tempete, 0, 0).
+event(6, tremblement_de_terre, 0, 0).
+event(7, guerre_civile, 0, 0).
+event(8, conflit_majeur, 0, 0).
+event(9, conflit_mineur, 0, 0).
+event(10, panne_courant, 0, 0).
+event(11, incendie, 0, 0).
+event(12, météorite, 0, 0).
+event(13, chute_boursiere, 0, 0).
+event(14, attaque_terroriste, 0, 0).
+event(15, accident_voitures, 0, 0).
+event(16, accident_avion, 0, 0).
+event(17, augmentation_taxes, 0, 0).
+event(18, braquage, 0, 0).
+event(19, scandale_pollution, 0, 0).
+event(20, bouchons, 0, 0).
